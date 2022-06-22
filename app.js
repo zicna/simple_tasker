ceef48b41b63d0f079c94ce083a3a0ba38cb9d61
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-06-22 15:33:13 -0500

    migrate all methods from ui.js filr to app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ class App {
 
   constructor() {
     this._getTasks()
+    this._UIdisplayTasks()
+    this._toggleShowClearBtn()
 
     // * Event Listeners
     taskAddNew.addEventListener('click', this._toggleShow)
@@ -17,12 +19,10 @@ class App {
   _toggleShow() {
     taskForm.classList.toggle('no-show')
     taskAddNew.classList.toggle('no-show')
-    taskClearAll.classList.toggle('no-show')
   }
-
+  // ! not sure about this if-else
   _toggleShowClearBtn() {
-    if (localStorage.getItem('tasks')?.length)
-      taskClearAll.classList.add('no-show')
+    if (this.#tasks.length == 0) taskClearAll.classList.add('no-show')
     else taskClearAll.classList.remove('no-show')
   }
 
@@ -38,6 +38,65 @@ class App {
     }, 2000)
   }
 
+  // ***********************************************
+  //* Handling Storage
+  _getTasks() {
+    const data = JSON.parse(localStorage.getItem('tasks'))
+    if (!data) return
+
+    this.#tasks = data
+  }
+
+  _addTask(task) {
+    this._getTasks()
+    this.#tasks.unshift(task)
+    localStorage.setItem('tasks', JSON.stringify(this.#tasks))
+  }
+
+  _removeTask(target) {
+    const taskID = target.closest('tr').dataset.id
+    this._getTasks()
+
+    this.#tasks = this.#tasks.filter((task) => task.id !== taskID)
+    localStorage.setItem('tasks', JSON.stringify(this.#tasks))
+  }
+
+  _clearAllTasks() {
+    localStorage.setItem('tasks', JSON.stringify([]))
+    this._getTasks()
+  }
+
+  // ***********************************************
+  // * UI methods
+  _UIdisplayTasks() {
+    this.#tasks.forEach((task) => this._UIaddTaskToTable(task))
+  }
+
+  _UIaddTaskToTable(task) {
+    const tableRow = document.createElement('tr')
+    tableRow.setAttribute('data-id', task.id)
+    tableRow.innerHTML = `
+                <th>${task.subject}</th>
+                <th>${task.body}</th>
+                <th>${formatDate(task.deadline)}</th>
+                <th>${formatDate(task.timestamp)}</th>
+                <th>${daysLeft(task.deadline, task.timestamp)}</th>
+                <th><a href="#" class="btn btn-danger">X</a></th>
+                <th><a href="#" class="btn btn-success">DONE</a></th>
+            `
+    taskList.prepend(tableRow)
+  }
+
+  _UIremoveTask(target) {
+    target.closest('tr').remove()
+  }
+
+  _UIclearAllTasks() {
+    taskList.innerHTML = ''
+  }
+
+  // ***********************************************
+  // * handle functions
   _handleSubmit(event) {
     event.preventDefault()
     this._toggleShow()
@@ -59,8 +118,9 @@ class App {
     }
     const task = new Task(taskS, taskB, taskD)
     this._addTask(task)
-    UI.addTaskToTable(task)
+    this._UIaddTaskToTable(task)
     this._taskNotification('success', 'new task has been created')
+    this._toggleShowClearBtn()
     event.target.reset()
   }
 
@@ -68,50 +128,24 @@ class App {
     event.preventDefault()
     event.target.parentElement.reset()
     this._toggleShow()
+    this._toggleShowClearBtn()
   }
 
   _handleRemove(event) {
     event.preventDefault()
-    // !Guard clause
+    //* Guard clause in case we click anywhere outside delete button
     if (!event.target.classList.contains('btn-danger')) return
 
     this._taskNotification('worning', 'task has been DELETED')
     this._removeTask(event.target)
-    UI.removeTask(event.target)
+    this._UIremoveTask(event.target)
+    this._toggleShowClearBtn()
   }
 
   _handleRemoveAll() {
-    UI.clearAllTasks()
+    this._UIclearAllTasks()
     this._clearAllTasks()
     this._taskNotification('danger', 'ALL tasks DELETED')
+    this._toggleShowClearBtn()
   }
-
-  // ***********************************************
-  //* Handling localStorage API
-
-  _getTasks() {
-    const data = JSON.parse(localStorage.getItem('tasks'))
-    // ! Guard clause
-    if (!data) return
-    this.#tasks = data
-  }
-
-  _addTask(task) {
-    this._getTasks()
-    this.#tasks.unshift(task)
-    localStorage.setItem('tasks', JSON.stringify(this.#tasks))
-  }
-
-  _removeTask(target) {
-    const taskID = target.closest('tr').dataset.id
-    this._getTasks()
-
-    this.#tasks = this.#tasks.filter((task) => task.id !== taskID)
-    localStorage.setItem('tasks', JSON.stringify(this.#tasks))
-  }
-
-  _clearAllTasks() {
-    localStorage.setItem('tasks', JSON.stringify([]))
-  }
-  // ***********************************************
 }
--- index.js
+++ index.js
@@ -16,105 +16,12 @@ const taskDate = document.getElementById('task-date')
 
 // ********************************************************
 
-// const toggleShowClearBtn = () => {
-//   if (localStorage.getItem('tasks')?.length)
-//     taskClearAll.classList.add('no-show')
-//   else taskClearAll.classList.remove('no-show')
-// }
-
-// * *****toggle show / no-show ****************
-// const toggleShow = () => {
-//   taskForm.classList.toggle('no-show')
-//   taskAddNew.classList.toggle('no-show')
-//   taskClearAll.classList.toggle('no-show')
-// }
-// * note notification container handler(ADDING, REMOVING, CLEARING)
-// const taskNotification = (action, message) => {
-//   notification.classList.toggle('no-show')
-//   notification.classList.toggle(`${action}`)
-//   notification.textContent = `${message}`
-
-//   setTimeout(() => {
-//     notification.classList.toggle('no-show')
-//     notification.classList.toggle(`${action}`)
-//     notification.textContent = ``
-//   }, 2000)
-// }
-// ******************* handle functions *************************************
-// const handleSubmit = (event) => {
-//   event.preventDefault()
-//   toggleShow()
-
-//   const taskS = taskSubject.value
-//   const taskB = taskBody.value
-//   const taskD = taskDate.value
-
-//   console.log(formatDate(taskD));
-
-//   if (taskS === '' || taskB === '' || taskD === '') {
-//     taskNotification(
-//       'worning',
-//       'all fields must be filled'
-//     )
-//   } else if (dateInPast(taskD)) {
-//     taskNotification(
-//       'worning',
-//       'can NOT choose date in past. Please try again.'
-//     )
-//   } else {
-//     const task = new Task(taskS, taskB, taskD)
-//     Storage.addTask(task)
-//     UI.addTaskToTable(task)
-//     taskNotification('success', 'new task has been created')
-//   }
-//   event.target.reset()
-// }
-
-// const handleCancel = (event) => {
-//   event.preventDefault()
-//   event.target.parentElement.reset()
-//   toggleShow()
-// }
-
-// const handleRemove = (event) => {
-//   event.preventDefault()
-//   // !Guard clause
-//   if (!event.target.classList.contains('btn-danger')) return
-
-//   taskNotification('worning', 'task has been DELETED')
-//   Storage.removeTask(event.target)
-//   UI.removeTask(event.target)
-// }
-
-// const handleRemoveAll = () => {
-//   UI.clearAllTasks()
-//   Storage.clearAllTasks()
-//   taskNotification('danger', 'ALL tasks DELETED')
-// }
-
 const handleBeforeunload = (event) => {
   event.preventDefault()
   localStorage.clear()
   event.returnValue = null
 }
 
-// *****************************************************************
-// // * Event Listeners
-// taskAddNew.addEventListener('click', toggleShow)
-// taskCancel.addEventListener('click', handleCancel)
-// taskForm.addEventListener('submit', handleSubmit)
-// taskList.addEventListener('click', handleRemove)
-// taskClearAll.addEventListener('click', handleRemoveAll)
-
 window.addEventListener('beforeunload', handleBeforeunload)
 
-const app = new App();
-console.log(app);
-
-// *******************
-// * dammy data
-const task1 = new Task("taskOne", "this is body of taskOne", "2022-06-25" )
-const task2 = new Task("taskTwo", "this is body of taskTwo", "2022-06-30" )
-// *******************
-
-
+const app = new App()
